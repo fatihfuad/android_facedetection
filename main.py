817df3d01a84db635aab791934ba074a6bdc9512
Ismailfatih98 <71960766+Ismailfatih98@users.noreply.github.com>
2021-05-24 15:28:06 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,76 @@
+#==========================================================================
+# system component
+
+import numpy as np
+import time
+import cv2
+import time
+import os,random
+import subprocess
+from jnius import autoclass, cast
+from android import activity, mActivity
+from android.permissions import request_permissions, Permission
+
+request_permissions([Permission.CAMERA, Permission.READ_EXTERNAL_STORAGE])
+
+Intent = autoclass('android.content.Intent')
+MediaStore = autoclass('android.provider.MediaStore')
+Environment = autoclass('android.os.Environment')
+Context = autoclass("android.content.Context")
+FileProvider = autoclass('android.support.v4.content.FileProvider')
+PythonActivity = autoclass("org.kivy.android.PythonActivity").mActivity
+
+#============================================================================
+# Kivy components
+
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+from kivy.graphics.texture import Texture
+from kivy.uix.camera import Camera
+from kivy.lang import Builder
+
+
+# create a parent class        
+
+
+
+Builder.load_file("myapplayout.kv")
+
+class AndroidCamera(Camera):
+    camera_resolution = (640, 480)
+    
+
+    def _camera_loaded(self, *largs):
+        self.texture = Texture.create(size=np.flip(self.camera_resolution), colorfmt='rgb')
+        self.texture_size = list(self.texture.size)
+
+    def on_tex(self, *l):
+        if self._camera._buffer is None:
+            return None
+        frame = self.frame_from_buf()
+        self.frame_to_screen(frame)
+        super(AndroidCamera, self).on_tex(*l)
+
+    def frame_from_buf(self):
+        w, h = self.resolution
+        frame = np.frombuffer(self._camera._buffer.tostring(), 'uint8').reshape((h + h // 2, w))
+        frame_bgr = cv2.cvtColor(frame, 93)
+        return np.rot90(frame_bgr, 3)
+
+    def frame_to_screen(self, frame):
+        frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        flipped = np.flip(frame_rgb, 0)
+        buf = flipped.tostring()
+        self.texture.blit_buffer(buf, colorfmt='rgb', bufferfmt='ubyte')
+
+class MyLayout(BoxLayout):
+    pass
+
+class MyApp(App):
+    def build(self):
+        return MyLayout()
+
+
+MyApp().run()
+
+    
